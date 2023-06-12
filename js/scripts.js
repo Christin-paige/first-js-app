a084f407990f147bbc8c120db6b9902831c506f8
Christin-paige <christinpmartin@gmail.com>
2023-06-12 08:41:49 -0400

    updated the pokemonList
    added 7 Pokemons to the pokemonList for task 1.2

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,7 @@
-let pokemonList = [];
+let pokemonList = [{name: 'Bulbasaur', height: 7, types:['grass', 'poison']},
+    {name: 'Ivysaur', height: 1, types:['grass', 'poison']},
+    {name: 'Venusaur', height: 2, types:['grass', 'poison']},
+    {name: 'Charmander', height: 0.6, types:['fire', 'steel']},
+    {name: 'Charmeleon', height: 1.1, types: ['steel', 'fire']},
+    {name: 'Charizard', height: 1.7, types:['ghost', 'flying', 'fire', 'steel']},
+    {name: 'Squirtle', height: 0.5, types: ['water', 'dark', 'steel']}];
