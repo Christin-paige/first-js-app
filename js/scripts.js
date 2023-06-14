0d0661ce9438f568e323e1e0b0186078d5955b1f
Christin-paige <christinpmartin@gmail.com>
2023-06-14 06:37:11 -0400

    added for loop
    added a "for" loop that iterates through the Pokemon array

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,17 @@
 let pokemonList = [{name: 'Bulbasaur', height: 7, types:['grass', 'poison']},
     {name: 'Ivysaur', height: 1, types:['grass', 'poison']},
     {name: 'Venusaur', height: 2, types:['grass', 'poison']},
-    {name: 'Charmander', height: 0.6, types:['fire', 'steel']},
-    {name: 'Charmeleon', height: 1.1, types: ['steel', 'fire']},
-    {name: 'Charizard', height: 1.7, types:['ghost', 'flying', 'fire', 'steel']},
-    {name: 'Squirtle', height: 0.5, types: ['water', 'dark', 'steel']}];
+    {name: 'Charmander', height: 0.6, types:['fire']},
+    {name: 'Charmeleon', height: 1.1, types: ['fire']},
+    {name: 'Charizard', height: 1.7, types:['flying', 'fire']},
+    {name: 'Squirtle', height: 0.5, types: ['water']}];
+
+    for (i=0; i<pokemonList.length; i++) {
+       document.write(pokemonList[i].name + ', ');
+    }
+
+    for (i=0; i<pokemonList.length; i++) {
+        document.write(pokemonList[i].name + ' (' + 'height: ' + pokemonList[i].height + '), ');
+
+    }
+    
